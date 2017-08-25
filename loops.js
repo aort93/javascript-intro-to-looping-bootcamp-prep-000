7494a097aed622cce84770ef5f8ab9b729ed0565
Alonso Ortega <alonsojortega@gmail.com>
2017-08-25 06:13:03 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 for(i=1; i<=25; i++){
   if (i===1){
-    console.log(`I am ${i} strange loop/\.`)
+    console.log(`I am ${i} strange loop.`)
 
   }else {
   console.log(`I am ${i} strange loops.`)
