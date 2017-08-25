cd3c28cb9ff017b33106dfeecc34110f1d065863
Alonso Ortega <alonsojortega@gmail.com>
2017-08-25 06:12:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,7 @@
-for(i=0; i<25; i++){
-  console.log(`I am ${i} strange loop.`)
+for(i=1; i<=25; i++){
+  if (i===1){
+    console.log(`I am ${i} strange loop/\.`)
+    
+  }else {
+  console.log(`I am ${i} strange loops.`)
 }
